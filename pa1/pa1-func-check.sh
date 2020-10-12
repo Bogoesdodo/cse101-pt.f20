b93e2a27fec1092d2916076c1e4ee6a926a782db
Hou-I Lin <47459281+houi-lin@users.noreply.github.com>
2020-10-12 13:26:01 -0700

    Update pa1-func-check.sh

--- pa1/pa1-func-check.sh
+++ pa1/pa1-func-check.sh
@@ -30,7 +30,7 @@ echo "Press enter to continue"
 read verbose
 for NUM in $(seq 1 $NUMTESTS); do
   rm -f outfile$NUM.txt
-  timeout 5 valgrind --leak-check=full -v Lex infile$NUM.txt outfile$NUM.txt &> valgrind-out$NUM.txt
+  timeout 5 valgrind --leak-check=full -v ./Lex infile$NUM.txt outfile$NUM.txt &> valgrind-out$NUM.txt
   diff -bBwu outfile$NUM.txt model-outfile$NUM.txt &> diff$NUM.txt >> diff$NUM.txt
   echo "Lex Test $NUM:"
   echo "=========="
